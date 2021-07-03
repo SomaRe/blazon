86412b40297eeea7aa9c7bf293b53da9f58222bc
SomaRe <shivatej82@gmail.com>
2021-07-03 21:40:42 +0530

    lot of chnages, color, images etc

--- blazon/script.js
+++ blazon/script.js
@@ -63,69 +63,6 @@ window.addEventListener("load", () => {
     "anim1 1s ease-in 1s forwards"
   );
 
-  //gallery
-  const preloader = document.querySelector(".preloader");
-
-  var Images = {
-    className: "works",
-    href: "blazon/assets/images/works/",
-    hrefCountArr: [[44, ".jpg"]],
-    src: "blazon/assets/images/works/",
-    srcCountArr: [[44, ".jpg"]],
-  };
-
-  document.getElementById("load-more").addEventListener("click", () => {
-    createHtml(Images);
-    document.getElementById("load-more").innerHTML = "";
-  });
-
-  function createHtml(obj) {
-    preloader.style.display = "flex";
-    preloader.classList.remove("vanish");
-
-    document.getElementById("gallery").style.display = "none";
-    document.getElementById("gallery").innerHTML = "";
-
-    var gridSizer = document.createElement("Div");
-    gridSizer.classList.add("grid-sizer");
-    document.getElementById("gallery").appendChild(gridSizer);
-
-    for (let j = 0; j < obj.srcCountArr.length; j++) {
-      for (let i = 1; i < obj.srcCountArr[j][0] + 1; i++) {
-        var imgBox = document.createElement("Div");
-        imgBox.classList.add("wrapper", obj.className);
-        document.getElementById("gallery").appendChild(imgBox);
-
-        var link = document.createElement("a");
-        link.classList.add("glightbox");
-        link.href = obj.href + i + obj.hrefCountArr[j][1];
-        imgBox.appendChild(link);
-
-        var imgTag = document.createElement("img");
-        imgTag.src = obj.src + i + obj.srcCountArr[j][1];
-        link.appendChild(imgTag);
-      }
-    }
-    initGlightbox();
-
-    Promise.all(
-      Array.from(document.images).map((img) => {
-        if (img.complete) return Promise.resolve(img.naturalHeight !== 0);
-        return new Promise((resolve) => {
-          img.addEventListener("load", () => resolve(true));
-          img.addEventListener("error", () => resolve(false));
-        });
-      })
-    ).then((results) => {
-      if (results.every((res) => res)) {
-        document.getElementById("gallery").style.display = "block";
-        preloader.classList.add("vanish");
-        runistopeLayout();
-        console.log("all images loaded successfully");
-      } else console.log("some images failed to load, all finished loading");
-    });
-  }
-
   function initGlightbox() {
     const lightbox = GLightbox({
       selector: ".glightbox",
