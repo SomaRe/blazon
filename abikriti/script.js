b8f342354fa7431fef9e18f9d80323ed28166e4c
SomaRe <shivatej82@gmail.com>
2021-09-04 23:06:01 +0530

    added gallery route

--- abikriti/script.js
+++ abikriti/script.js
@@ -4,3 +4,30 @@ window.addEventListener('scroll', e => {
     topPattern.style.backgroundPosition = scrollY/10+"px";
     bottomPattern.style.backgroundPosition = scrollY/10+"px";
 })
+
+initGlightbox();
+
+runistopeLayout();
+
+function initGlightbox() {
+    const lightbox = GLightbox({
+      selector: ".glightbox",
+    });
+  }
+  
+
+function runistopeLayout() {
+    var grid = document.querySelector(".gallery-container");
+    var iso = new Isotope(grid, {
+      // options...
+      itemSelector: ".wrapper",
+      percentPosition: true,
+      masonry: {
+        columnWidth: ".grid-sizer",
+      },
+    });
+    imagesLoaded(grid).on("progress", function () {
+      // layout Isotope after each image loads
+      iso.layout();
+    });
+  }
